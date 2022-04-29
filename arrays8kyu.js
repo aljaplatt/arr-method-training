f609257141ef764074231f79cafbf476c063f3f3
Alex Platt <alja.platt@gmail.com>
2022-04-29 17:31:24 +0100

    add length

--- arrays8kyu.js
+++ arrays8kyu.js
@@ -894,3 +894,17 @@ function addLength(str) {
 function printArray(array) {
 	return array.join();
 }
+
+//* Implement Array.prototype.filter() 
+
+Array.prototype.filter = function( ruleCondition ) {
+	// Create an empty array to store the values that satisfy the ruleCondition
+	  var arr = [];
+	  
+	  this.forEach( function( currentValue ) {
+	  // Evaluate each element and push the value when content in array meet condition
+		if( ruleCondition( currentValue ) ) arr.push( currentValue );
+	  });
+	  return arr;
+	}
+	
