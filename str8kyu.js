5fd88a1e17a0b3458aa3fbb73c72424e502b02ab
Alex Platt <alja.platt@gmail.com>
2022-05-11 14:44:33 +0100

    returning strings

---
+++ str8kyu.js
@@ -0,0 +1,17 @@
+//? Convert a Number to a String!
+
+function numberToString(num) {
+	// Return a string of the number here!
+	return String(num);
+}
+var stringToNumber = function (str) {
+	// put your code here
+	return Number(str);
+};
+
+//? Returning Strings
+
+function greet(name) {
+	//your code here
+	return `Hello, ${name} how are you doing today?`;
+}
