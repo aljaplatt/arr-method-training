ef6a541ae97b788e0f89d4c1406d2467f0cb9bee
Alex Platt <alja.platt@gmail.com>
2022-05-23 11:19:26 +0100

    L1: Bartender, drinks!

--- str8kyu.js
+++ str8kyu.js
@@ -133,3 +133,26 @@ function stringClean(s) {
 		.filter(n => n != parseInt(n))
 		.join('');
 }
+
+//? L1: Bartender, drinks!
+
+function getDrinkByProfession(param) {
+	param = param.toLowerCase();
+
+	switch (param) {
+		case 'jabroni':
+			return 'Patron Tequila';
+		case 'school counselor':
+			return 'Anything with Alcohol';
+		case 'programmer':
+			return 'Hipster Craft Beer';
+		case 'bike gang member':
+			return 'Moonshine';
+		case 'politician':
+			return 'Your tax dollars';
+		case 'rapper':
+			return 'Cristal';
+		default:
+			return 'Beer';
+	}
+}
