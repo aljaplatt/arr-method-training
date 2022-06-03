a4bcfbe5f29c09d33c454054ebdbfa35a5131706
Alex Platt <alja.platt@gmail.com>
2022-06-03 13:43:43 +0100

    Define a card suit

--- str8kyu.js
+++ str8kyu.js
@@ -192,3 +192,24 @@ var repeatIt = function (str, n) {
 	}
 	return stringVar;
 };
+
+//? Define a card suit
+
+function defineSuit(card) {
+	if(card.includes('♥')) return 'hearts'
+	if(card.includes('♦')) return 'diamonds'
+	if(card.includes('♣')) return 'clubs'
+	if(card.includes('♠')) return 'spades' 
+	}
+
+	//* other solution
+
+	// function defineSuit(card) {
+	// 	const s = {
+	// 	  "♣": "clubs",
+	// 	  "♠": "spades",
+	// 	  "♦": "diamonds",
+	// 	  "♥": "hearts"
+	// 	}
+	// 	return s[card.charAt(card.length - 1)]
+	//   }
